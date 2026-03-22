78e13865d741782d3e2cf102a0bb73bf5ebf338c
nikitor141 <nikitor141@gmail.com>
2026-03-22 12:50:58 +0300

    Improved getAsObject function | Switched to cqi unit in product-card

--- src/utils/getAsObject.ts
+++ src/utils/getAsObject.ts
@@ -1,16 +1,17 @@
-export function getAsObject(map: Map<any, any> | object): {} {
-	const obj = {}
-	if (map instanceof Map) {
-		for (let [key, value] of map) {
-			obj[key] = value instanceof Map ? getAsObject(value) : value instanceof Set ? Array.from(value) : value
-		}
-	}
-	if (typeof map === 'object') {
-		for (let key in map) {
-			const value = map[key]
-			obj[key] = value instanceof Map ? getAsObject(value) : value instanceof Set ? Array.from(value) : value
-		}
+function convertValue(value: unknown): unknown {
+	if (value instanceof Map) return getAsObject(value)
+	if (value instanceof Set) return Array.from(value, convertValue)
+	if (Array.isArray(value)) return value.map(convertValue)
+	return value
+}
+
+export function getAsObject(item: Map<unknown, unknown> | object): Record<string, unknown> {
+	const obj: Record<string, unknown> = {}
+	const iterable: Iterable<[any, unknown]> =
+		Symbol.iterator in item ? (item as Iterable<[unknown, unknown]>) : Object.entries(item)
+
+	for (let [key, value] of iterable) {
+		obj[key] = convertValue(value)
 	}
 	return obj
 }
-//todo что-то тут не так, скорее всего не глубокое преобразование в объект. Использовать Object.fromEntries?
